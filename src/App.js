2f13ff58666f277bfd3f57412878240e0e21ac9e
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-06-27 21:19:24 +0300

    Done on the nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navigation/Navbar';
+import Home from './components/home/Home';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <>
+        <Navbar/>
+        <Home/>
+      </>
+    
     </div>
   );
 }
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+import homeImg from '../../assets/ferry1.jpg'
+function Home() {
+  return (
+    <div className="hero-page">
+        <img src={homeImg} alt='Heros page'/>
+    </div>
+  )
+}
+
+export default Home
