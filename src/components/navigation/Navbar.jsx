25a92b521534d3dc2cdc1b9611b1115c1073514e
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-06-28 13:44:43 +0300

    Heros page

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/Logo.png'
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/Logo.png";
 function Navbar() {
   return (
     <div className="navbar">
-        <img src={logo} alt='Logo' className='logo'/>
-        <h1>SAFARIBROS <span>SHIPPING</span></h1>
-        <ul>
-            <li>Home</li>
-             <li>About</li>
-              <li>Our Products</li>
-               <li>Our Services</li>
-                <li>Contacts</li>
-        </ul>
+      <div className="navbar-left">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1 className="brand">
+          SAFARIBRO'S <span>SHIPPING</span>
+        </h1>
+      </div>
+
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>About</li>
+        <li>Our Products</li>
+        <li>Our Services</li>
+        <li>Contacts</li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
