e3788bd3e48ded484445825713dba0358d69510c
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-06-28 20:58:24 +0300

    Heros page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,7 +4,15 @@ import homeImg from '../../assets/ferry1.jpg'
 function Home() {
   return (
     <div className="hero-page">
-        <img src={homeImg} alt='Heros page'/>
+            <p className="tagline">SHIP FROM TEXAS TO KENYA</p>
+        <h1>
+          Your Number One Trusted <br /> Shipping Company
+        </h1>
+        <p className="description">
+          We guarantee that your commodities will be delivered as scheduled.
+          For the many years we have been in the shipping business from Texas to Kenya,
+          we have never lost an item and our customers have been very satisfied.
+        </p>
     </div>
   )
 }
